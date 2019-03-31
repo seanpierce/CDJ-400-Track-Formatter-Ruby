1d84ac244addcb8d48c84ef4b5f83039b4a7777f
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-30 20:34:20 -0700

    Import and leverage track class methods

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require "id3tag"
 require 'fileutils'
 
+require_relative 'track'
+
 mp3s = Dir.entries("./").select { |filename| filename =~ /\.mp3/i }
 
 mp3s.each do |file|
 
 	begin
 		ID3Tag.read(File.open(file, "rb")) do |tag|
-			FileUtils.mkdir_p tag.artist
-			FileUtils.mv(file, "#{tag.artist}/#{tag.title}.mp3")
+			track = Track.new(tag)
+			FileUtils.mkdir_p "Artists/#{track.artist}"
+			FileUtils.mv(file, "Artists/#{track.artist}/#{track.title}.mp3")
 		end
 	rescue StandardError => e
 		puts "An error occurred: #{e.message}"
