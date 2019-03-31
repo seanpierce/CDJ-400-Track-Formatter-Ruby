569916c108f40a3a6d6539329465f3a67f0a4fea
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-30 20:34:01 -0700

    Add Track class

---
+++ track.rb
@@ -0,0 +1,29 @@
+class Track
+	def initialize(tag)
+		@tag = tag
+	end
+
+	def artist
+		if !@tag.artist
+			return "Unknown Artist"
+		else
+			if @tag.artist.include? "/" 
+				return @tag.artist.gsub("/", "_") 
+			else 
+				return @tag.artist 
+			end
+		end
+	end
+
+	def title
+		if !@tag.title
+			return "Unknown Title"
+		else
+			if @tag.title.include? "/" 
+				return @tag.title.gsub("/", "_") 
+			else 
+				return @tag.title 
+			end
+		end
+	end
+end
