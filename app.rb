1a2231287a641a9e981e74e8ae5c692326e18e01
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-03-30 19:32:25 -0700

    Implement error handling

--- app.rb
+++ app.rb
@@ -4,11 +4,15 @@ require 'fileutils'
 mp3s = Dir.entries("./").select { |filename| filename =~ /\.mp3/i }
 
 mp3s.each do |file|
-	ID3Tag.read(File.open(file, "rb")) do |tag|
 
-		artist = tag.artist
-		title = tag.title
-		FileUtils.mkdir_p artist
-		FileUtils.mv(file, "#{artist}/#{title}.mp3")
+	begin
+		ID3Tag.read(File.open(file, "rb")) do |tag|
+			FileUtils.mkdir_p tag.artist
+			FileUtils.mv(file, "#{tag.artist}/#{tag.title}.mp3")
+		end
+	rescue StandardError => e
+		puts "An error occurred: #{e.message}"
+	ensure
+		next
 	end
 end
